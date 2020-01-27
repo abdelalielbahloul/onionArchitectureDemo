5e05c78bff4ab304cd12f4bd8e935dc79f939397
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-01-27 14:25:13 +0100

    create an interface called irepository and implement all its methods

--- Repo/IRepository.cs
+++ Repo/IRepository.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Repo
 {
-    interface IRepository
+    public interface IRepository<T> where T : BaseEntity
     {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Remove(T entity);
+        void SaveChanges();
+
+
     }
 }
---
+++ Repo/Repository.cs
@@ -0,0 +1,85 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repo
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+        private DbSet<T> _entities;
+        public Repository(AppDbContext context)
+        {
+            this._context = context;
+            this._entities = context.Set<T>();
+        }
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Empty");
+            }
+            else
+            {
+                _entities.Remove(entity);
+                _context.SaveChanges();
+            }
+        }
+
+        public T Get(int id)
+        {
+            return _entities.SingleOrDefault(entity => entity.Id == id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.AsEnumerable();
+        }
+
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Empty");
+            }
+            else 
+            {
+                _entities.Add(entity);
+                _context.SaveChanges();
+            }
+        }
+
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Empty");
+            }
+            else
+            {
+                _context.Remove(entity);
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Empty");
+            }
+            else
+            {
+                _entities.Update(entity);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
